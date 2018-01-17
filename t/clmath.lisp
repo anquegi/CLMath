80c6f1e6c863c9431680f8d5b8552ffe9bf57d6f
anquegi <antonio.querol@cuaqea.com>
2018-01-17 12:57:51 +0100

    added tests for horner package

--- t/clmath.lisp
+++ t/clmath.lisp
@@ -2,10 +2,17 @@
 
 (defpackage clmath-test
   (:use :cl
-   :parachute
+        :parachute
         :clmath.consts
         :clmath.horner))
 
 (in-package :clmath-test)
+
+(define-test "Horner package is a tool for evaluating polynomials"
+  (of-type list  (poly-expand 't '(1 2 3)))
+  (is equal '(+ 0 (* X (+ 0 (* X (+ 0 (* X 1)))))) (poly-expand 'x '(0 0 0 1)) )
+  (is = 8 (poly 2 0 0 0 1))
+  (is = 16 (horner-hack (isqrt 16) 0 0 1)))
+
 
 
